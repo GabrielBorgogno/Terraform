bd1602dc91256a8547380d8bd19916ebe4c09c61
GabrielBorgogno <geduardae@gmail.com>
2024-09-14 09:47:37 -0300

    fix dependencies

--- deploy_infra.tf
+++ deploy_infra.tf
@@ -1,16 +1,14 @@
-# Configure o provedor de cloud (neste caso, o Kubernetes)
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-# Crie um namespace para o monitoramento
 resource "kubernetes_namespace" "monitoring" {
   metadata {
     name = "monitoring"
   }
 }
 
-# Instale o Helm no cluster
+
 resource "helm_release" "helm" {
   name       = "helm"
   repository = "https://charts.helm.sh/stable"
@@ -18,8 +16,6 @@ resource "helm_release" "helm" {
   version    = "3.5.4"
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
 }
-
-# Instale o Prometheus com Helm
 resource "helm_release" "prometheus" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
@@ -28,11 +24,10 @@ resource "helm_release" "prometheus" {
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
   values = [
-    file("${path.module}/values/prometheus-values.yaml")
+    "https://raw.githubusercontent.com/prometheus-community/helm-charts/main/charts/prometheus/values.yaml"
   ]
 }
 
-# Instale o Grafana com Helm
 resource "helm_release" "grafana" {
   name       = "grafana"
   repository = "https://grafana.github.io/helm-charts"
@@ -41,11 +36,10 @@ resource "helm_release" "grafana" {
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
   values = [
-    file("${path.module}/values/grafana-values.yaml")
+    "https://raw.githubusercontent.com/grafana/helm-charts/main/charts/grafana/values.yaml"
   ]
 }
 
-# Crie um ServiceAccount para o Prometheus
 resource "kubernetes_service_account" "prometheus" {
   metadata {
     name      = "prometheus"
@@ -53,7 +47,6 @@ resource "kubernetes_service_account" "prometheus" {
   }
 }
 
-# Crie um ClusterRole para o Prometheus
 resource "kubernetes_cluster_role" "prometheus" {
   metadata {
     name = "prometheus"
@@ -66,7 +59,6 @@ resource "kubernetes_cluster_role" "prometheus" {
   }
 }
 
-# Crie um ClusterRoleBinding para o Prometheus
 resource "kubernetes_cluster_role_binding" "prometheus" {
   metadata {
     name = "prometheus"
