f15ddcd30c696a88f09b483e572084b49d25869a
GabrielBorgogno <geduardae@gmail.com>
2024-09-14 12:05:15 -0300

    added grafana and prometheus on the stack from helm

---
+++ providers.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "kubectl" {
+  load_config_file = false
+  config_path = "~/.kube/config"
+}
